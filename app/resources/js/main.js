3303b209482c035d3e9d706fbb6d77073bb37c4c
Brett Marsh <brett.m.coding@gmail.com>
2019-09-07 21:55:50 -0400

    added quest recolorization, custom designed png buttons, wow SFX, sound checkbox

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -1,3 +1,9 @@
+// TODO: Are you sure you want to abandon this quest?
+// TODO: subcategories?
+// TODO: login / save mechanism
+// TODO: turn off sound button
+// TODO: random xp
+
 let data = (localStorage.getItem('todoList')) ? JSON.parse                      (localStorage.getItem('todoList')) : {
       todo: [],
       completed: []
@@ -5,9 +11,27 @@ let data = (localStorage.getItem('todoList')) ? JSON.parse                      
 
 console.log(JSON.parse(localStorage.getItem('todoList')));
 
-// Remove/Complete button's SVG code
-let removeSVG = '<svg style="enable-background:new 0 0 22 22"version=1.1 viewBox="0 0 22    22"x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><rect class=noFill height=22 width=22 /><g><g><path class=fill d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3 c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9 C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7 c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2 c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class=fill d=M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z /></g><g><path class=fill d=M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z /></g><g><path class=fill d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8 C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-let completeSVG =  '<svg style="enable-background:new 0 0 22 22"version=1.1 viewBox="0 0 22 22"x=0px xml:space=preserve xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink y=0px><rect class=noFill height=22 width=22 y=0 /><g><path class=fill d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+let audioOn = true;
+let audioCheck = document.getElementById("audiocheckbox");
+
+let audioClick = new Audio("../app/resources/assets/sfx/click.mp3");
+let audioQuestComplete = new Audio("../app/resources/assets/sfx/questcomplete.mp3");
+let audioQuestAdded = new Audio("../app/resources/assets/sfx/questadded.mp3");
+let audioQuestAbandon = new Audio("../app/resources/assets/sfx/questabandoned.mp3");
+
+let audioCheckBoxElement = document.getElementById("audiocheckbox");
+audioCheckBoxElement.addEventListener('change', toggleAudio);
+
+
+function toggleAudio() {
+  if (audioCheck.checked == true) {
+    console.log("CHECKED IS TRUE");
+    audioOn = true;
+  } else {
+    console.log("CHECKED IS FALSE");
+    audioOn = false;
+  }
+}
 
 renderTodoList();
 
@@ -37,6 +61,10 @@ function addItem(value) {
       
       data.todo.push(value);
       dataObjectUpdated();
+      if (audioOn) {
+        audioClick.play();
+        audioQuestAdded.play();
+      }
 }
 
 function renderTodoList() {
@@ -73,6 +101,11 @@ function removeItem() {
   dataObjectUpdated();
 
   parent.removeChild(item);
+
+  if (audioOn) {
+    audioClick.play();
+    audioQuestAbandon.play();
+  }
 }
 
 function completeItem() {
@@ -97,6 +130,10 @@ function completeItem() {
 
   parent.removeChild(item);
   target.insertBefore(item, target.childNodes[0]);
+  if (audioOn) {
+    audioClick.play();
+    audioQuestComplete.play();
+  }
 }
 
 function addItemToDOM(text, completed) {
@@ -110,14 +147,14 @@ function addItemToDOM(text, completed) {
 
   let remove = document.createElement('button');
   remove.classList.add('remove')
-  remove.innerHTML = removeSVG;
+  //remove.innerHTML = removePNGbtn;
 
   // Add click event for removing item
   remove.addEventListener('click', removeItem);
   
   let complete = document.createElement('button');
   complete.classList.add('complete')
-  complete.innerHTML = completeSVG;
+  //complete.innerHTML = completeSVG;
 
   // Add click event for completing the item
   complete.addEventListener('click', completeItem);
